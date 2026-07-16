927fb385d2974bab690822b0ea86b71a6c007f0e
tiantian-WL <850725039@qq.com>
2026-07-16 20:05:35 +0800

    Create index.js

---
+++ zijian18/worker/src/index.js
@@ -0,0 +1,18 @@
+export default {
+  async fetch(request) {
+
+    return new Response(
+      JSON.stringify({
+        code: 1,
+        msg: "Worker运行成功",
+        time: new Date().toISOString()
+      }),
+      {
+        headers:{
+          "content-type":"application/json;charset=UTF-8"
+        }
+      }
+    );
+
+  }
+}
