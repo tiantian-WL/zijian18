850b2e3fd5bddd67212645d0cc57021298169a99
tiantian-WL <850725039@qq.com>
2026-07-16 22:49:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-1
+
+
+export default {
+  async fetch(request) {
+
+    return new Response(
+      JSON.stringify({
+        code: 1,
+        msg: "Worker运行成功",
+        time: new Date().toISOString()
+      }),
+      {
+        headers:{
+          "content-type":"application/json;charset=UTF-8"
+        }
+      }
+    );
+
+  }
+}
