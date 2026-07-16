5fb81eae8fea40e432013f6fc99fa2b26d0b8dd2
tiantian-WL <850725039@qq.com>
2026-07-16 22:41:15 +0800

    Implement fetch function to return JSON response

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-123
+
+export default {
+  async fetch(request) {
+
+    return new Response(
+      JSON.stringify({
+        code: 1,
+        msg: "Worker运行成功",
+        time: new Date().toISOString()
+      }),
+      {
+        headers:{
+          "content-type":"application/json;charset=UTF-8"
+        }
+      }
+    );
+
+  }
+}
