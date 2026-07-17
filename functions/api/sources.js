2503a52c0c2bf23eaad18916961dc67e9e6e6cdf
tiantian-WL <850725039@qq.com>
2026-07-17 22:14:19 +0800

    Update sources.js

--- functions/api/sources.js
+++ functions/api/sources.js
@@ -1,8 +1,11 @@
 export async function onRequest(context) {
 
-  const response = await context.env.ASSETS.fetch(
-    new Request("https://fake/config/sources.json")
+  const url = new URL(
+    "/config/sources.json",
+    context.request.url
   );
+
+  const response = await fetch(url);
 
   const data = await response.json();
 
@@ -10,7 +13,8 @@ export async function onRequest(context) {
     JSON.stringify(data),
     {
       headers: {
-        "content-type": "application/json;charset=UTF-8"
+        "content-type": "application/json;charset=UTF-8",
+        "cache-control": "no-cache"
       }
     }
   );
