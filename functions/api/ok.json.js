e29bf7975dccb98619078d0c6799c04ecbc4a053
tiantian-WL <850725039@qq.com>
2026-07-17 21:32:52 +0800

    Create ok.json.js

---
+++ functions/api/ok.json.js
@@ -0,0 +1,25 @@
+export async function onRequest() {
+
+  const data = {
+    sites: [
+      {
+        key: "155",
+        name: "155资源",
+        type: 1,
+        api: "https://155api.com/api.php/provide/vod",
+        playUrl: "",
+        search: 1
+      }
+    ]
+  };
+
+  return new Response(
+    JSON.stringify(data),
+    {
+      headers:{
+        "content-type":"application/json;charset=UTF-8"
+      }
+    }
+  );
+
+}
