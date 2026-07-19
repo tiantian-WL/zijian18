e2373a4af20b65857dfc6a460a30b7ea09f3f19a
tiantian-WL <850725039@qq.com>
2026-07-19 20:10:33 +0800

    Update spiders.js

--- functions/api/config/spiders.js
+++ functions/api/config/spiders.js
@@ -1,11 +1,11 @@
 export const spiders = [
 
   {
-    key: "ck",
-    name: "CK爬虫",
-    file: "/spiders/ck.js",
-    enable: true,
-    order: 1
+    key: "634",
+    name: "634TV",
+    file: "https://raw.githubusercontent.com/tiantian-WL/zijian18/main/public/spiders/🔞634TV资源.js",
+    order: 1,
+    enable: true
   }
 
 ];
