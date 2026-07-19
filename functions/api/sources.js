87560dd1044a349a3f77d9b2f140cec0135b5cc8
tiantian-WL <850725039@qq.com>
2026-07-19 09:46:36 +0800

    Update sources.js

--- functions/api/sources.js
+++ functions/api/sources.js
@@ -1,42 +1,17 @@
+import { sites } from "./config/sites";
+
 export async function onRequest() {
 
-  const data = {
+  const data = sites.map(item => ({
+    name: item.name,
+    type: item.type,
+    api: item.api
+  }));
+
+  return Response.json({
     code: 1,
     msg: "ok",
-    data: [
-      {
-        name: "155资源",
-        type: "api",
-        api: "https://155api.com/api.php/provide/vod"
-      },
-
-   {
-    name:"非凡资源",
-    type:"api",
-    api:"https://cj.ffzyapi.com/api.php/provide/vod"
-   },
-{
-  name: "🔞奶香香", 
-  type:"api",
-  api: "https://naixxzy.com/api.php/provide/vod"},
-      
-      
-      {
-        name: "CK资源",
-        type: "api",
-        api: "https://ckzy.me/api.php/provide/vod"
-      }
-    ]
-  };
-
-  return new Response(
-    JSON.stringify(data),
-    {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-        "cache-control": "no-cache"
-      }
-    }
-  );
+    data
+  });
 
 }
