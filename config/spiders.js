7a57c4748f655d3d1c81a63e43e359222149aaf8
tiantian-WL <850725039@qq.com>
2026-07-19 20:52:27 +0800

    Update spiders.js

--- config/spiders.js
+++ config/spiders.js
@@ -1,11 +1,11 @@
 export const spiders = [
 
-  {
-    key: "634",
-    name: "634TV",
-    file: "https://raw.githubusercontent.com/tiantian-WL/zijian18/main/public/spiders/🔞634TV资源.js",
-    order: 1,
-    enable: true
-  }
+{
+key:"634tv",
+name:"634TV资源",
+file:"/spiders/🔞634TV资源.js",
+order:1,
+enable:true
+}
 
 ];
