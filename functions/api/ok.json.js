ce15d5f4c8c0155805227e183aa89be7606e3756
tiantian-WL <850725039@qq.com>
2026-07-19 15:19:09 +0800

    Update ok.json.js

--- functions/api/ok.json.js
+++ functions/api/ok.json.js
@@ -1,11 +1,21 @@
-const okSites = sites
-  .filter(site => site.enable)
-  .sort((a, b) => a.order - b.order)
-  .map(site => ({
-    key: site.key,
-    name: site.name,
-    type: 1,
-    api: site.api,
-    playUrl: "",
-    search: 1
-  }));
+import { sites } from "./config/sites";
+
+export async function onRequest() {
+
+  const okSites = sites
+    .filter(site => site.enable)
+    .sort((a, b) => a.order - b.order)
+    .map(site => ({
+      key: site.key,
+      name: site.name,
+      type: 1,
+      api: site.api,
+      playUrl: "",
+      search: 1
+    }));
+
+  return Response.json({
+    sites: okSites
+  });
+
+}
