9a838d24823f19682af2ad422cb8dc7cb5b4ee24
tiantian-WL <850725039@qq.com>
2026-07-19 14:27:17 +0800

    Update sources.js

--- functions/api/sources.js
+++ functions/api/sources.js
@@ -1,17 +1,8 @@
-import { sites } from "./config/sites";
-
-export async function onRequest() {
-
-  const data = sites.map(item => ({
-    name: item.name,
-    type: item.type,
-    api: item.api
+const data = sites
+  .filter(site => site.enable)
+  .sort((a, b) => a.order - b.order)
+  .map(site => ({
+    name: site.name,
+    type: site.type,
+    api: site.api
   }));
-
-  return Response.json({
-    code: 1,
-    msg: "ok",
-    data
-  });
-
-}
