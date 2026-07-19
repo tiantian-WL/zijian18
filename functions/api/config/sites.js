8eb42ba273d89a49215d884990f7412f23c03f1d
tiantian-WL <850725039@qq.com>
2026-07-19 09:42:49 +0800

    Create sites.js

---
+++ functions/api/config/sites.js
@@ -0,0 +1,17 @@
+export const sites = [
+
+  {
+    key: "155",
+    name: "155资源",
+    type: "api",
+    api: "https://155api.com/api.php/provide/vod"
+  },
+
+  {
+    key: "ck",
+    name: "CK资源",
+    type: "api",
+    api: "https://ckzy.me/api.php/provide/vod"
+  }
+
+];
