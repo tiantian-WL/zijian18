c6d2077272ebc2cb8ab2620fe58732ebe6fb43d1
tiantian-WL <850725039@qq.com>
2026-07-20 20:44:26 +0800

    Create tvbox.js

---
+++ config/tvbox.js
@@ -0,0 +1,12 @@
+export const tvbox = [
+
+  {
+    key:"634tv",
+    name:"634TV资源",
+    url:"https://zijian18.pages.dev/public/spiders/634TV-TVBox.js",
+    type:"js",
+    enable:true,
+    order:1
+  }
+
+];
