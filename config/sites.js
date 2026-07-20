df9627e3c047cbf26e488f493567314536878343
tiantian-WL <850725039@qq.com>
2026-07-20 22:32:22 +0800

    Update sites.js

--- config/sites.js
+++ config/sites.js
@@ -18,12 +18,23 @@ order:2,
 enable:true
 },
 
+
+{key:"tgzy", name:"TG资源", type:"api", api:"https://tgzyz.pp.ua/api.php/provide/vod", order:11, enable:true },
+  { key:"ynzy", name:"越南", type:"api", api:"https://vnzyz.com/api.php/provide/vod", order:12, enable:true }, 
+  { key:"askzy", name:"奥斯卡", type:"api", api:"https://aosikazy4.com/api.php/provide/vod", order:3, enable:true }, 
+  { key:"xxbzy", name:"X细胞", type:"api", api:"https://www.xxibaozyw.com/api.php/provide/vod", order:4, enable:true }, 
+  { key:"91av", name:"91AV", type:"api", api:"https://91av.cyou/api.php/provide/vod", order:5, enable:true }, 
+  { key:"91jp", name:"91精品", type:"api", api:"https://91jpzyw.com/api.php/provide/vod/at/json", order:6, enable:true }, 
+  { key:"xjzy", name:"小鸡", type:"api", api:"https://api.xiaojizy.live/provide/vod", order:7, enable:true }, 
+  { key:"dnzzy", name:"大奶子", type:"api", api:"https://apidanaizi.com/api.php/provide/vod", order:8, enable:true }, 
+  { key:"ddzy", name:"滴滴", type:"api", api:"https://api.ddapi.cc/api.php/provide/vod", order:9, enable:true }, 
+  { key:"jkzy", name:"鸡坤", type:"api", api:"https://jkunzyapi.com/api.php/provide/vod", order:10, enable:true },
 {
 key:"ff",
 name:"非凡资源",
 type:"api",
 api:"https://cj.ffzyapi.com/api.php/provide/vod",
-order:3,
+order:13,
 enable:true
 }
 
